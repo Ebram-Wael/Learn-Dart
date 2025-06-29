286b895b86bb8b43c0f46978944d6265e62d5ddc
Ebram-Wael <202227393@std.sci.cu.edu.eg>
2025-06-30 01:24:17 +0300

    convert user input

--- userInput.dart
+++ userInput.dart
@@ -9,4 +9,10 @@ void main(){
   print("Enter your name: ");
   var name2 = stdin.readLineSync();
   print("Your name is $name2");
+
+  //convert string to integer
+  print("Enter a number: ");
+  var num = stdin.readLineSync(); 
+  var number = int.parse(num ?? "0"); // ?? means that the value can be null , if the value is null then it will be 0
+  print("Your age is $number");
 }
